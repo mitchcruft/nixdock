e1cc2381e6d243e3ee1c131b8cd75ce6041e96f8
mitchcruft <mitchcruft@users.noreply.github.com>
2024-11-09 15:47:35 -0800

    refactor folder

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd $(dirname $0)
+cd ..
 
 set -ex
-docker build -t chesterfied/localdev .
+docker build . -t chesterfied/localdev -f docker/Dockerfile
 docker run \
   --hostname arch \
   -it \
