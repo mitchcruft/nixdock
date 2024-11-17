a77df4a3270d40eb4a07c87d48e65dc3d9a0810c
mitchcruft <mitchcruft@users.noreply.github.com>
2024-11-17 01:32:16 -0800

    be able to call scripts from home-manager

--- home-manager/env.nix
+++ home-manager/env.nix
@@ -4,9 +4,10 @@ let
   # TODO: Determine which to use based on the installation?
   singleNixProfile = "$HOME/.nix-profile/bin";
   multiNixProfile = "/etc/profiles/per-user/${username}/bin";
+  homeManagerConfig = "$HOME/.config/home-manager";
   swBin = "/run/current-system/sw/bin";
   userBin = "$HOME/bin";
 in
 {
-  PATH = "${singleNixProfile}:${multiNixProfile}:${swBin}:${userBin}:$PATH";
+  PATH = "${singleNixProfile}:${multiNixProfile}:${swBin}:${userBin}:${homeManagerConfig}:$PATH";
 }
