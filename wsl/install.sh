3409af3097a1ea7273694390562e67bd248a2c38
mitchcruft <mitchcruft@users.noreply.github.com>
2024-11-17 09:54:54 -0800

    add wsl installer

---
+++ wsl/install.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -ex
+
+# Initialize pacman
+pacman-key --init
+pacman-key --populate
+pacman --noconfirm -Sy archlinux-keyring
+pacman --noconfirm -Su
+
+# Install git
+pacman --noconfirm -S git
+
+# Install zsh
+pacman --noconfirm -S zsh
+
+# Install nix
+sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+
+# Add users
+useradd -m -G nixbld,wheel -s /bin/zsh m
+
+# Set up sudo
+echo '%wheel ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/01_wheel
+
+# Set up wsl
+echo '
+[network]
+hostname = "batcave"
+
+[user]
+default = "m"
+' > /etc/wsl.conf
+
+# Install home-manager
+sudo -u m sh -c '
+set -ex
+mkdir ~/.config
+git clone http://github.com/mitchcruft/nixdock /home/m/.config/home-manager
+PATH="/nix/var/nix/profiles/default/bin:${PATH}" nix --extra-experimental-features "flakes nix-command" run home-manager/release-24.05 -- switch
+'
+
+# Install yay
+pacman --noconfirm -S base-devel fakeroot
+sudo -u m sh -c '
+cd /tmp
+git clone https://aur.archlinux.org/yay-bin.git
+cd /tmp/yay-bin
+makepkg -si --noconfirm
+cd /tmp
+rm -rf yay-bin
+'
+
+# Login as m
+exec su m
