10c04c3306ff7d490364a74229a9fe63e1794bf3
Mitch Cruft <mitchcruft@gmail.com>
2024-11-24 15:42:14 -0800

    ensure setup runs as root

--- wsl/install-ubuntu.bat
+++ wsl/install-ubuntu.bat
@@ -3,15 +3,17 @@ wsl --terminate Ubuntu
 wsl --unregister Ubuntu
 
 :: Install Ubuntu
-echo | wsl --install Ubuntu
+:: TODO: Figure out how to avoid user input here
+wsl --install Ubuntu
 
 :: Set Ubuntu as default
 wsl -s Ubuntu
+wsl -t Ubuntu
 
 :: Download and run post-installation script
-wsl -d Ubuntu curl -Lo "/tmp/mach-setup.sh" "https://raw.githubusercontent.com/mitchcruft/nixdock/refs/heads/main/bin/mach-setup.sh"
-wsl -d Ubuntu sh /tmp/mach-setup.sh
+wsl -u root -d Ubuntu curl -Lo "/tmp/mach-setup.sh" "https://raw.githubusercontent.com/mitchcruft/nixdock/refs/heads/main/bin/mach-setup.sh"
+wsl -u root -d Ubuntu bash /tmp/mach-setup.sh --wsl --hostname wsl-ubuntu --user m --ubuntu
 
-# Reset wsl to force settings update
+:: Reset wsl to force settings update
 wsl -t Ubuntu
 wsl -d Ubuntu
