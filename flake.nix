400c72dc1a6efd52acdfd967413159760b94388b
mitchcruft <mitchcruft@users.noreply.github.com>
2024-11-24 23:52:46 -0800

    use linux by default

--- flake.nix
+++ flake.nix
@@ -1,123 +1,33 @@
 
 {
-  description = "nix-darwin system flake";
+  description = "Home manager configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-homebrew = {
-      url = "github:zhaofengli-wip/nix-homebrew";
-    };
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
 
   };
 
-  outputs = inputs@{
-    self, nix-darwin, nixpkgs, home-manager, nix-homebrew, homebrew-core,
-    homebrew-cask, ...
-  }:
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
   let
-    system = "aarch64-darwin";
+    system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     stateVersion = "24.05";
     username = "m";
-    homeDirectory = "/Users/m";
-    hostname = "janeair";
-    darwinConfiguration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = [];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before
-      # changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "${system}";
-    };
+    homeDirectory = "/home/m";
   in
   {
-    darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
-
+    homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
       modules = [
-        darwinConfiguration
-        home-manager.darwinModules.home-manager
-        {
-          homebrew = {
-            enable = true;
-            casks = [
-             # "alt-tab"
-             # "orbstack"
-            ];
-          };
-          users.users.${username} = {
-            name = username;
-            home = homeDirectory;
-            isHidden = false;
-            shell = self.darwinPackages.zsh;
-          };
-	  home-manager = (import ./home-manager/default.nix {
-	    pkgs = self.darwinPackages;
-	    inherit stateVersion username homeDirectory;
-	  }).home-manager;
-        }
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            # Install Homebrew under the default prefix
-            enable = true;
-
-            # Apple Silicon Only: Also install Homebrew under the default
-            # Intel prefix for Rosetta 2
-            # TODO: should be based on x86_64 vs aarch64
-            enableRosetta = false;
-
-            # User owning the Homebrew prefix
-            user = username;
-
-            # Optional: Declarative tap management
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-            };
-
-            # Optional: Enable fully-declarative tap management
-            #
-            # With mutableTaps disabled, taps can no longer be added
-            # imperatively with `brew tap`.
-            mutableTaps = false;
-          };
-        }
+        ./home.nix
       ];
     };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations.${hostname}.pkgs;
   };
+
 }
 
