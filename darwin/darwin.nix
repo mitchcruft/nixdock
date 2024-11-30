f10c2147619c6b93fd31f1fa4a06e85d76c92aac
mitchcruft <mitchcruft@users.noreply.github.com>
2024-11-29 23:31:30 -0800

    move darwin to separate file

---
+++ darwin/darwin.nix
@@ -0,0 +1,77 @@
+{ pkgs, hostConfig, home-manager, nix-darwin, nix-homebrew, homebrew-core, homebrew-cask, configurationRevision }:
+
+with hostConfig; {
+  ${hostname} = nix-darwin.lib.darwinSystem {
+
+    modules = [
+      {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = [];
+
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        nix.package = pkgs.nix;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = configurationRevision;
+
+        # Used for backwards compatibility, please read the changelog before
+        # changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = system;
+      }
+      home-manager.darwinModules.home-manager {
+        homebrew = {
+          enable = true;
+          casks = [
+           # "alt-tab"
+           # "orbstack"
+          ];
+        };
+        users.users.${username} = {
+          name = username;
+          home = homeDirectory;
+          isHidden = false;
+          shell = pkgs.zsh;
+        };
+        home-manager.users.${username} = import ../home-manager/home.nix {
+          inherit pkgs hostConfig;
+        };
+      }
+      nix-homebrew.darwinModules.nix-homebrew
+      {
+        nix-homebrew = {
+          # Install Homebrew under the default prefix
+          enable = true;
+
+          # Apple Silicon Only: Also install Homebrew under the default
+          # Intel prefix for Rosetta 2
+          # TODO: should be based on x86_64 vs aarch64
+          enableRosetta = false;
+
+          # User owning the Homebrew prefix
+          user = username;
+
+          # Optional: Declarative tap management
+          taps = {
+            "homebrew/homebrew-core" = homebrew-core;
+            "homebrew/homebrew-cask" = homebrew-cask;
+          };
+
+          # Optional: Enable fully-declarative tap management
+          #
+          # With mutableTaps disabled, taps can no longer be added
+          # imperatively with `brew tap`.
+          mutableTaps = true;
+        };
+      }
+    ];
+  };
+}
--- flake.nix
+++ flake.nix
@@ -35,97 +35,21 @@
   in with hostConfig;
   {
 
-   homeConfigurations = if isStandalone then {
-     ${hostConfig.username} = home-manager.lib.homeManagerConfiguration {
-       inherit pkgs;
-       modules = [
-         ({ ... }: import ./home-manager/home.nix {
-           inherit pkgs hostConfig;
-	 })
-       ];
-       #home-manager.users.${username} = import ./home-manager/home.nix {
-       #};
-     };
-   } else null;
-
-    darwinConfigurations = if hostConfig.isDarwin then {
-      ${hostname} = nix-darwin.lib.darwinSystem {
-
+    homeConfigurations = if isStandalone then {
+      ${username} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
         modules = [
-          {
-            # List packages installed in system profile. To search by name, run:
-            # $ nix-env -qaP | grep wget
-            environment.systemPackages = [];
-
-            # Auto upgrade nix package and the daemon service.
-            services.nix-daemon.enable = true;
-            nix.package = pkgs.nix;
-
-            # Necessary for using flakes on this system.
-            nix.settings.experimental-features = "nix-command flakes";
-
-            # Set Git commit hash for darwin-version.
-            system.configurationRevision = self.rev or self.dirtyRev or null;
-
-            # Used for backwards compatibility, please read the changelog before
-            # changing.
-            # $ darwin-rebuild changelog
-            system.stateVersion = 5;
-
-            # The platform the configuration will be used on.
-            nixpkgs.hostPlatform = system;
-          }
-          home-manager.darwinModules.home-manager {
-            homebrew = {
-              enable = true;
-              casks = [
-               # "alt-tab"
-               # "orbstack"
-              ];
-            };
-            users.users.${username} = {
-              name = username;
-              home = homeDirectory;
-              isHidden = false;
-              shell = self.darwinPackages.zsh;
-            };
-            home-manager.users.${username} = import ./home-manager/home.nix {
-              inherit pkgs hostConfig;
-            };
-          }
-          nix-homebrew.darwinModules.nix-homebrew
-          {
-            nix-homebrew = {
-              # Install Homebrew under the default prefix
-              enable = true;
-
-              # Apple Silicon Only: Also install Homebrew under the default
-              # Intel prefix for Rosetta 2
-              # TODO: should be based on x86_64 vs aarch64
-              enableRosetta = false;
-
-              # User owning the Homebrew prefix
-              user = username;
-
-              # Optional: Declarative tap management
-              taps = {
-                "homebrew/homebrew-core" = homebrew-core;
-                "homebrew/homebrew-cask" = homebrew-cask;
-              };
-
-              # Optional: Enable fully-declarative tap management
-              #
-              # With mutableTaps disabled, taps can no longer be added
-              # imperatively with `brew tap`.
-              mutableTaps = true;
-            };
-          }
+          ({ ... }: import ./home-manager/home.nix {
+            inherit pkgs hostConfig;
+ 	 })
         ];
       };
     } else null;
 
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = if hostConfig.isDarwin then self.darwinConfigurations.${hostname}.pkgs else null;
+    darwinConfigurations = if isDarwin then import ./darwin/darwin.nix {
+      inherit pkgs hostConfig home-manager nix-darwin nix-homebrew homebrew-core homebrew-cask;
+      configurationRevision = self.rev or self.dirtyRev or null;
+    } else null;
 
   };
 }
