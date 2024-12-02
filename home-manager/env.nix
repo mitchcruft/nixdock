f153242d062a9d211f725df0203be5deea130c69
mikecurtis <mikecurtis@users.noreply.github.com>
2024-12-01 21:40:48 -0800

    add color for eza and fzf

--- home-manager/env.nix
+++ home-manager/env.nix
@@ -18,4 +18,9 @@ in
   # Use bat for manpages
   MANPAGER = "sh -c 'col -bx | bat -l man -p'";
   MANROFFOPT = "-c";
+
+  # Use dracula for eza and fzf
+  EZA_COLORS = "uu=36:gu=37:sn=32:sb=32:da=34:ur=34:uw=35:ux=36:ue=36:gr=34:gw=35:gx=36:tr=34:tw=35:tx=36:";
+  FZF_DEFAULT_OPTS = "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4";
+
 }
