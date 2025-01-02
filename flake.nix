66051d9314a73374fca6789d0dc8f86ef9626816
mikecurtis <mikecurtis@users.noreply.github.com>
2025-01-02 07:41:21 -0600

    update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "home-manager / nix-darwin configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
@@ -22,7 +22,6 @@
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
-
   };
 
   outputs = inputs@{
