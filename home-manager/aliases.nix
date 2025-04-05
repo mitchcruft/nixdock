3a54f6dcd40a75a1dfbed6bb9d4a58bb9c7af700
mitchcruft <mitchcruft@users.noreply.github.com>
2025-04-05 12:10:26 -0700

    add aliases to cd / pull configs

--- home-manager/aliases.nix
+++ home-manager/aliases.nix
@@ -1,20 +1,26 @@
 { isStandalone, isDarwin }:
 
 let
-  hmMake = "make -C " + (
-    if isStandalone then
+
+  hmDir = if isStandalone then
       "$HOME/.config/home-manager"
     else if isDarwin then
       "$HOME/.config/nix-darwin"
     else
       throw "home-manager only supported in standalone or nix-darwin"
-  );
+    ;
+
+  hmMake = "make -C " + hmDir;
 in
 {
   cat = "bat";
   ch = "container-home.sh";
   view = "nvim -R";
 
+  # cd to home-manager/nix-darwin
+  hmc = "cd " + hmDir;
+  # git pull
+  hmp = "(cd " + hmDir + "&& git pull)";
   # Switch home-manager/nix-darwin
   hms = hmMake + " && unalias -a && unset -m '*_SOURCED' && exec $SHELL";
   # Dry run witch home-manager/nix-darwin
