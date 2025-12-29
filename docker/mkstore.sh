7ca22bc13c1eb62a21f0b415e5e79ecb7486ecaf
mitchcruft <mitchcruft@users.noreply.github.com>
2025-12-28 18:56:27 -0800

    fix nixhome

--- docker/mkstore.sh
+++ docker/mkstore.sh
@@ -1,5 +1,10 @@
 #!/root/.nix-profile/bin/bash
 
+[ $HOME ] || (
+  echo "no HOME" >&2
+  exit 1
+)
+
 set -ex
 
 STORE=$1
@@ -10,7 +15,7 @@ fi
 
 mkdir -p ${STORE}
 
-ROOT="$(dirname $(dirname $(readlink /home/m/.cache/.keep)))"
+ROOT="$(dirname $(dirname $(readlink $HOME/.cache/.keep)))"
 
 for storedir in $(nix-store --query --requisites ${ROOT}); do
   cp -ar ${storedir} ${STORE}
